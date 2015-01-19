b67773fc399353012c8412ac162014de693522c3
Steve Buzonas <steve@fancyguy.com>
2015-01-19 01:58:07 -0500

    fix typo in config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.color = true
-  confog.raise_errors_for_deprecations!
+  config.raise_errors_for_deprecations!
   config.filter_run :focus
 
   config.before(:each) do
